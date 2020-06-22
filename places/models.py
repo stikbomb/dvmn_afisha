6ffbb0dee8c8e253e6770cc1ddc77924a36940f5
stikbomb <stikbomb@gmail.com>
2020-06-23 00:44:27 +0300

    added place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Краткое описание', blank=True)
+    description_long = models.TextField('Полное описание', blank=True)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+
+    def __str__(self):
+        return f"{self.title}"
