2d50cb7009581334412c27a254ac0e19c1020f72
stikbomb <stikbomb@gmail.com>
2020-06-22 19:21:28 +0300

    Add full frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'where-to-go-frontend-master')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
