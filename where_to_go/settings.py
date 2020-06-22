51f99f49f49705dc00563b1a9d9aa754c8a516dc
stikbomb <stikbomb@gmail.com>
2020-06-22 17:42:38 +0300

    bare frontend added, views func fixed

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'where_to_go.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'where-to-go-frontend-master')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
