6aca91582a7364abfc63c2868222b32074aaba53
stikbomb <stikbomb@gmail.com>
2020-06-24 13:08:20 +0300

    database connected to frontend, geojson

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
-    return render(request, '../templates/index.html')
+    places = Place.objects.all()
+
+    feature_collection = make_feature_collection(places)
+
+    data = {'feature_collection': feature_collection}
+
+    return render(request, '../templates/index.html', context=data)
+
+
+def make_feature_collection(places):
+    result = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for place in places:
+        feature = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lon, place.lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.id,
+            "detailsUrl": "zagluwe4ka"
+          }
+        }
+
+        result["features"].append(feature)
+
+    return result
